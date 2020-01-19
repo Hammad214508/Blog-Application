687141cc19396e4c403f5e8fd09d7aff6abf0c2b
Hammad <hammadmuhammad15@gmail.com>
2020-01-19 14:25:10 +0000

    database intead of dummy data

--- project/blog/models.py
+++ project/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+# Database
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    # auto_now=True date will change each time it is updated implement it for last updated
+    # auto_now_add=True Cannot be changed so date created
+    date_posted = models.DateTimeField(default=timezone.now)
+    # Delete user then delete post
+    author= models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title 
--- project/blog/views.py
+++ project/blog/views.py
@@ -2,28 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 # Create a  function to manage the traffic from the home
+from .models import Post
 # Takes a request and return what you want the user to see
 
-
-posts = [
-    {
-        'author': 'Hammad',
-        'title': 'blog post',
-        'content': 'Frist post',
-        'date_posted': '17 January 2020'
-    },
-    {
-        'author': 'Aadya',
-        'title': 'blog post',
-        'content': 'Second post',
-        'date_posted': '11 March 2020'
-    }
-]
+# Dummy data
+# posts = [
+#     {
+#         'author': 'Hammad',
+#         'title': 'blog post',
+#         'content': 'Frist post',
+#         'date_posted': '17 January 2020'
+#     },
+#     {
+#         'author': 'Aadya',
+#         'title': 'blog post',
+#         'content': 'Second post',
+#         'date_posted': '11 March 2020'
+#     }
+# ]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
