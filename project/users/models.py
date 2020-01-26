77442692956b73fde2693bdfbd2beeadef5006e7
Hammad <hammadmuhammad15@gmail.com>
2020-01-26 13:08:44 +0000

    resize the profile picture and picture shown in posts

--- project/users/models.py
+++ project/users/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 # Create your models here.
-#
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    # it runs after model is saved, we are overriding
+    def save(self):
+        super().save() #Run the save for the parent class
+        # Grab the image and resize it
+        img = Image.open(self.image.path)
+        if img.width > 300 or img.height > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
